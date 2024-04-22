84d20c8f6788bec7fac91b7a69b75b59adb99797
brian xu <blx2@cwru.edu>
2024-04-21 22:55:00 -0400

    additions to content.js
    added function to generate XPath of an element
    added functions to allow disabling of document links in selection mode (to prevent clicking on links and redirecting page)

--- myextension/scripts/content.js
+++ myextension/scripts/content.js
@@ -25,6 +25,7 @@ const tags = [
 let curr = null;
 let selectorEnabled = false;
 
+// determine hovered node
 document.addEventListener('mouseover', (e) => {
 
     if (!selectorEnabled) {
@@ -57,6 +58,27 @@ document.addEventListener('mouseover', (e) => {
     false
 );
 
+// for disabling and enabling links on document
+function disableLinks() {
+    getA().pointerEvents = 'none';
+}
+function enableLinks() {
+    getA().pointerEvents = '';
+}
+function getA() {
+    for (let i = 0; i < document.styleSheets.length; i++) {
+        let styleSheet = document.styleSheets[i];
+        let rules = styleSheet.cssRules;
+        for (let j = 0; j < rules.length; j++) {
+            let rule = rules[j];
+            if (rule.selectorText === 'a') {
+                return(rule.style);
+            }
+        }
+    }
+    return 0;
+}
+
 document.addEventListener('click', async () => {
     console.log('you clicked content while selector was %s', selectorEnabled);
     if (!selectorEnabled) return;
@@ -67,6 +89,7 @@ document.addEventListener('click', async () => {
         name:       curr.className,
         tagname:    curr.tagName.toLowerCase(),
         classlist:  curr.classList,
+        path: getXPathForElement(curr),
         },
         response => {
             console.log("content got response", response);
@@ -78,9 +101,32 @@ chrome.runtime.onMessage.addListener(
 
         if (request.name === 'selectToggle') {
             selectorEnabled = !selectorEnabled;
+
+            if (selectorEnabled) disableLinks();
+            else enableLinks();
+
             sendResponse({
                 selectStatus:   selectorEnabled,
             })
         }
     }
 )
+
+function getXPathForElement(element) {
+    const idx = (sib, name) => sib 
+        ? idx(sib.previousElementSibling, name||sib.localName) + (sib.localName == name)
+        : 1;
+    const segs = elm => !elm || elm.nodeType !== 1 
+        ? ['']
+        : elm.id && document.getElementById(elm.id) === elm
+            ? [`id("${elm.id}")`]
+            : [...segs(elm.parentNode), `${elm.localName.toLowerCase()}[${idx(elm)}]`];
+    return segs(element).join('/');
+}
+
+function getElementByXPath(path) { 
+    return (new XPathEvaluator()) 
+        .evaluate(path, document.documentElement, null, 
+                        XPathResult.FIRST_ORDERED_NODE_TYPE, null) 
+        .singleNodeValue; 
+} 
